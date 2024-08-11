ae6c344701a7e3105bc56748c9b1abb42a0e4c4d
Jamie-PROG <jamiefiskerjensenok@gmail.com>
2024-08-11 18:39:01 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add any interactivity here, like smooth scrolling or animations.
+document.querySelectorAll('.link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
