8e07803e4365ce63b4fbdbdc2e9fd711a902c00c
Jamie-PROG <jamiefiskerjensenok@gmail.com>
2024-08-11 18:49:25 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add any interactivity here, like smooth scrolling or animations.
-document.querySelectorAll('.link').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
